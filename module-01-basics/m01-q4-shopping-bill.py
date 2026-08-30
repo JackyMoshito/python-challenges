5bd13bd485f6e88529edbb000306e9484f3f27f2
JackyMoshito <147833172+JackyMoshito@users.noreply.github.com>
2026-08-30 08:55:17 +0200

    incomplete shopping bill

---
+++ module-01-basics/m01-q4-shopping-bill.py
@@ -0,0 +1,10 @@
+#Enter 3 items and their prices
+product_1 = input("Enter item 1: ")
+product_1_price = int(input("Enter the price for item 1: "))
+
+product_2 = input("Enter item 2: ")
+product_2_price = int(input(Enter the price for item 2: ))
+
+product_3 = input("Enter itme 3: ")
+product_3_price = int(input("Enter the price for item 3: "))
+
